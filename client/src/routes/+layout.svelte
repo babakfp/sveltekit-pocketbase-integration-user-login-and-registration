bcd69909aa9035d0e4bf656dc758683e26e05a38
babakfp <44144724+babakfp@users.noreply.github.com>
2023-01-14 09:35:20 -0800

    Added the login page

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,25 +1,25 @@
 <script>
-  import "../app.postcss"
-  import { currentUser } from '$lib/pocketbase'
+	import { applyAction, enhance } from "$app/forms"
+	import { currentUser, pb } from "$lib/pocketbase"
+	import "../app.postcss"
 </script>
 
 <header class="border-b border-gray-100">
 	<div class="container px-0 flex justify-between h-20">
-		<a class="flex items-center h-full px-4" href="/">
-			SkitBase
-		</a>
+		<a class="flex items-center h-full px-4" href="/">Home</a>
 		<div class="flex">
 			{#if $currentUser}
-				<a class="flex items-center h-full px-4" href="/logout">
-					Logout
-				</a>
+				<form method="POST" action="/logout" use:enhance={() => {
+					return async ({ result }) => {
+						pb.authStore.clear()
+						await applyAction(result)
+					}
+				}}>
+					<button class="flex items-center h-full px-4">Logout</button>
+				</form>
 			{:else}
-				<a class="flex items-center h-full px-4" href="/login">
-					Login
-				</a>
-				<a class="flex items-center h-full px-4" href="/register">
-					Register
-				</a>
+				<a class="flex items-center h-full px-4" href="/login">Login</a>
+				<a class="flex items-center h-full px-4" href="/register">Register</a>
 			{/if}
 		</div>
 	</div>
---
+++ client/src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { enhance, applyAction } from '$app/forms'
+	import { pb } from '$lib/pocketbase'
+</script>
+
+<form class="grid gap-4" method="POST" use:enhance={() => {
+	return async ({ result }) => {
+		pb.authStore.loadFromCookie(document.cookie)
+		await applyAction(result)
+	}
+}}>
+	<input class="w-full h-14 px-4 bg-gray-100 rounded" type="email" name="email" id="email" placeholder="Email" />
+	<input class="w-full h-14 px-4 bg-gray-100 rounded" type="password" name="password" id="password" placeholder="Password" />
+	<button class="h-16 bg-gray-900 rounded text-white">Login</button>
+</form>
