7ca8ee2331046bf47ca0a6264b80adc9baa034dc
babakfp <44144724+babakfp@users.noreply.github.com>
2023-01-15 01:50:28 -0800

    Fixed header container x spacing and links hover

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <header class="border-b border-gray-100">
-	<div class="container px-0 flex justify-between h-20">
-		<a class="flex items-center h-full px-4" href="/">Home</a>
+	<div class="container flex justify-between h-20">
+		<a class="flex items-center h-full px-4 hover:bg-slate-100" href="/">Home</a>
 		<div class="flex">
 			{#if $currentUser}
 				<form method="POST" action="/logout" use:enhance={() => {
@@ -15,11 +15,11 @@
 						await applyAction(result)
 					}
 				}}>
-					<button class="flex items-center h-full px-4">Logout</button>
+					<button class="flex items-center h-full px-4 hover:bg-slate-100">Logout</button>
 				</form>
 			{:else}
-				<a class="flex items-center h-full px-4" href="/login">Login</a>
-				<a class="flex items-center h-full px-4" href="/register">Register</a>
+				<a class="flex items-center h-full px-4 hover:bg-slate-100" href="/login">Login</a>
+				<a class="flex items-center h-full px-4 hover:bg-slate-100" href="/register">Register</a>
 			{/if}
 		</div>
 	</div>
